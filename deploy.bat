58cb7dcd5c63b8d9f6263e6eba05683d2c0d17de
Liushimin-dili <877481515@qq.com>
2026-03-05 01:50:33 +0800

    Clean and update

--- deploy.bat
+++
@@ -1,8 +0,0 @@
-cd /d C:\Users\Administrator\diLi-xuexi-platform
-"C:\Program Files\Git\cmd\git.exe" init
-"C:\Program Files\Git\cmd\git.exe" config user.email "877481515@qq.com"
-"C:\Program Files\Git\cmd\git.exe" config user.name "Liushimin-dili"
-"C:\Program Files\Git\cmd\git.exe" remote add origin https://github.com/Liushimin-dili/diLi-xuexi-platform.git
-"C:\Program Files\Git\cmd\git.exe" add .
-"C:\Program Files\Git\cmd\git.exe" commit -m "Add 初中地理学习平台"
-"C:\Program Files\Git\cmd\git.exe" push origin main
